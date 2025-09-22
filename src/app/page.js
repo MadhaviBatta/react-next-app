747f2b8b22881b31fcc4129dc8ec3e9f6e110431
MadhaviBatta <batta.madhavi@gmail.com>
2025-09-22 06:54:58 +0000

    reactnextapp

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+   return (
+    <div>
+      <h2>Welcome to My Website</h2>
+      <p>This is the home page content.</p>
+    </div>
+  );
+}
